a798a92f7b83d9e2c59690e14ab6d0d001056faf
flyingpenguin999 <flying.penguin9196@gmail.com>
2024-03-21 10:36:07 -0400

    login api complete

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -16,6 +17,7 @@ app.use(cors());
 //     res.json({ message: "Hello from express endpoint!" });
 // });
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 app.listen(7000, () => {
