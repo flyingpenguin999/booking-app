cc0ce169d0b30f39758e05719b3628400863e3c7
flyingpenguin999 <flying.penguin9196@gmail.com>
2024-03-20 23:24:33 -0400

    tailwindcss, prettier setting internal, react-route-dom

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '127.0.0.1'
-  }
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
 })
