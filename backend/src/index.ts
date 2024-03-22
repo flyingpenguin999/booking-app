13ecc69b09059ae89444787e4305a6d73cc2c190
flyingpenguin999 <flying.penguin9196@gmail.com>
2024-03-22 01:05:28 -0400

    cors solve by localhost,

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,9 +10,13 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-// origin: process.env.FRONTEND_URL,
-// credentials: true,
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
 // API testing function
 // app.get("/api/test", async (req: Request, res: Response) => {
 //     res.json({ message: "Hello from express endpoint!" });
